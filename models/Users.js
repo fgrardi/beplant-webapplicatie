47d647ae5025ed0f99617ed9674c873038cf0118
SarahOorts <r0657122@student.thomasmore.be>
2021-05-10 18:08:05 +0200

    feat: create models folder and users.js file, add mongoose schema to models/users.js file

---
+++ models/Users.js
@@ -0,0 +1,9 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+const passportLocalMongoose = require("passport-local-mongoose");
+
+const User = new Schema({});
+
+User.plugin(passportLocalMongoose, {usernameField: "email"});
+
+module.exports = mongoose.model("User", User);
