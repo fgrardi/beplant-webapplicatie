ca71c97e6acbb4608207a5dc094030f114a2198e
SarahOorts <r0657122@student.thomasmore.be>
2021-05-10 17:40:13 +0200

    fix: update app.js with userRouter for routes to routes/api/v1/users.js

--- app.js
+++ app.js
@@ -6,6 +6,7 @@ var logger = require('morgan');
 
 var indexRouter = require('./routes/index');
 //
+const userRouter = require("./routes/api/v1/users");
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
@@ -19,6 +20,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 //
+app.use("/users", userRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
