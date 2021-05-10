81d1ba67351a371fe22fb42d0d304571ba4d99f2
SarahOorts <r0657122@student.thomasmore.be>
2021-05-10 19:30:02 +0200

    feat: add passport folder & passport.js, add setup content to passport.js

---
+++ passport/passport.js
@@ -0,0 +1,11 @@
+const passport = require("passport");
+const User = require("./../models/Users"); 
+
+// CHANGE: USE "createStrategy" INSTEAD OF "authenticate"
+passport.use(User.createStrategy());
+
+//serialize user data for sessions
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser());
+
+
