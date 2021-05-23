381c369fc80c39f67a03ff1e8cb3a5d2762a5386
SarahOorts <r0657122@student.thomasmore.be>
2021-05-23 17:34:23 +0200

    feat: add clickeventlistener on specific event div to redirect to event_detail.html in public/javascripts/home.js. add clickeventlistener on specific workshop div to redirect to workshop_detail.html in public/javascripts/home.js

--- public/javascripts/home.js
+++ public/javascripts/home.js
@@ -17,9 +17,11 @@ window.addEventListener("load", function(){
             console.log("done1");
             return response.json();
         }).then(json =>{
-            if(json.status === "success"){
+            
+            // eventf(json);
+            if(json.status === "Success"){
                 console.log(json.data);
-                
+
                 json.data.forEach(function(e){
                     console.log(e.datum);
                     let date = splitdate(e.datum);
@@ -27,7 +29,7 @@ window.addEventListener("load", function(){
 
                     let titel = e.titel;
 
-                    let events = `<div><img src="" alt=""></div>
+                    let events = `<div><div><img src="" alt=""></div>
                     <div><p>${date[2] + " " + date[1]}</p></div>
                     <div><h3>${titel}</h3>
                     <p>Evenement</p>
@@ -35,7 +37,24 @@ window.addEventListener("load", function(){
                     </div></div>`;
 
                     document.querySelector(".events").innerHTML += events;
-                });
+                }); 
+                    // console.log(json);
+                    let eventData = json.data;
+                    // console.log(eventData);
+                    let eventsdetail = document.querySelector(".events");
+                    eventsdetail.addEventListener("click", (e)=>{
+                        let index = [... eventsdetail.childNodes].indexOf(e.target.parentElement.parentElement) -3; // /3 -1
+                        console.log(index);
+                        if(index < 0){index = null}
+                        if(index != null){
+                            console.log(eventData);
+                            console.log(eventData[index]);
+                            console.log(eventData[1]._id);
+                            localStorage.setItem("eventID", JSON.stringify(eventData[index]._id));
+                            window.location.replace("event_detail.html");
+                            console.log(ev);
+                        }
+                    });
             }
         })
 
@@ -49,15 +68,17 @@ window.addEventListener("load", function(){
             console.log("done2");
             return response.json();
         }).then(json =>{
-            if(json.status === "success"){
+            // workshopf(json);
+
+            if(json.status === "Success"){
                 console.log(json.data);
-                
+
                 json.data.forEach(function(e){
                     console.log(e.datum);
                     let date = splitdate(e.datum);
                     let titel = e.titel;
 
-                    let workshops = `<div><img src="" alt=""></div>
+                    let workshops = `<div><div><img src="" alt=""></div>
                     <div><p>${date[2] + " " + date[1]}</p></div>
                     <div><h3>${titel}</h3>
                     <p>Workshop</p>
@@ -65,10 +86,26 @@ window.addEventListener("load", function(){
                     </div></div>`;
 
                     document.querySelector(".workshops").innerHTML += workshops;
+
+                   
+                    let workshopData = json.data;
+                    let workshopdetail = document.querySelector(".workshops");
+                    workshopdetail.addEventListener("click", (e)=>{
+                        let index = [... workshopdetail.childNodes].indexOf(e.target.parentElement.parentElement)-3;
+                        console.log(index);
+                        if(index < 0){index = null}
+                        if(index != null){
+                            console.log(workshopData);
+                            console.log(workshopData[index]);
+                            console.log(workshopData[1]._id);
+                            localStorage.setItem("workshopID", JSON.stringify(workshopData[index]._id));
+                            window.location.replace("workshop_detail.html");
+                            console.log(workshopData);
+                        }
+                    });
                 });
             }
         })
-
     }
 });
 
@@ -79,9 +116,11 @@ function splitdate(date){
     if(!datum[0]){datum[0] = " ";}
     if(!datum[1]){datum[1] = " ";}
     if(!datum[2]){datum[2] = " ";}
-    console.log(" 0 " + datum[0]);
-    console.log( " 1 " + datum[1]);
-    console.log( " 2 " + datum[2]);
+    // console.log(" 0 " + datum[0]);
+    // console.log( " 1 " + datum[1]);
+    // console.log( " 2 " + datum[2]);
 
     return datum;
 }
+
+
