64270099fc05429e6b0ff3cc654559c39b5d6886
SarahOorts <r0657122@student.thomasmore.be>
2021-05-23 10:31:31 +0200

    feat: add put request for reset stappenplan to routes/api/v1/step.js

--- routes/api/v1/steps.js
+++ routes/api/v1/steps.js
@@ -3,7 +3,8 @@ var router = express.Router();
 const stepController = require("../../../controllers/api/v1/step");
 
 router.get("/", stepController.getSteps);
-router.post("/stappenplan", stepController.poststep);
-router.put("/stappenplan", stepController.putstep);
+// router.post("/stappenplan", stepController.poststep);
+router.put("/stappenplan/:id", stepController.putstep);
+router.put("/stappenplanreset/:id", stepController.putreset);
 
 module.exports = router;
