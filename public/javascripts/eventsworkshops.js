745e16b795d04864064d0f8c26451e14b22e7570
SarahOorts <r0657122@student.thomasmore.be>
2021-05-25 21:23:28 +0200

    feat: add API fetch for get event and get workshops to public/javascripts/eventsworkshops.js with redirect to event_detail.html & workshop_detail.html

--- public/javascripts/eventsworkshops.js
+++ public/javascripts/eventsworkshops.js
@@ -1,7 +1,7 @@
+let tokencheck = localStorage.getItem("token");
 window.addEventListener("load", function(){
-    let tokencheck = localStorage.getItem("token");
+    
     if(!tokencheck){
-        alert("wrong page");
         window.location.replace("login.html");
     }
     else{
@@ -17,17 +17,19 @@ window.addEventListener("load", function(){
             console.log("done1");
             return response.json();
         }).then(json =>{
+            
+            // eventf(json);
             if(json.status === "Success"){
                 console.log(json.data);
-                
+
                 json.data.forEach(function(e){
-                    console.log(e.datum);
+                    // console.log(e.datum);
                     let date = splitdate(e.datum);
                     console.log("ok");
 
                     let titel = e.titel;
 
-                    let events = `<div><img src="" alt=""></div>
+                    let events = `<div><div><img src="" alt=""></div>
                     <div><p>${date[2] + " " + date[1]}</p></div>
                     <div><h3>${titel}</h3>
                     <p>Evenement</p>
@@ -35,7 +37,24 @@ window.addEventListener("load", function(){
                     </div></div>`;
 
                     document.querySelector(".events").innerHTML += events;
-                });
+                }); 
+                    // console.log(json);
+                    let eventData = json.data;
+                    // console.log(eventData);
+                    let eventsdetail = document.querySelector(".events");
+                    eventsdetail.addEventListener("click", (e)=>{
+                        let index = [... eventsdetail.childNodes].indexOf(e.target.parentElement.parentElement) -19; // /3 -1 -3
+                        console.log(index);
+                        if(index < 0){index = null}
+                        if(index != null){
+                            console.log(eventData);
+                            // console.log(eventData[index]);
+                            // console.log(eventData[1]._id);
+                            localStorage.setItem("eventID", JSON.stringify(eventData[index]._id));
+                            window.location.replace("event_detail.html");
+                            // console.log(ev);
+                        }
+                    });
             }
         })
 
@@ -49,15 +68,17 @@ window.addEventListener("load", function(){
             console.log("done2");
             return response.json();
         }).then(json =>{
+            // workshopf(json);
+
             if(json.status === "Success"){
-                console.log(json.data);
-                
+                // console.log(json.data);
+
                 json.data.forEach(function(e){
-                    console.log(e.datum);
+                    // console.log(e.datum);
                     let date = splitdate(e.datum);
                     let titel = e.titel;
 
-                    let workshops = `<div><img src="" alt=""></div>
+                    let workshops = `<div><div><img src="" alt=""></div>
                     <div><p>${date[2] + " " + date[1]}</p></div>
                     <div><h3>${titel}</h3>
                     <p>Workshop</p>
@@ -65,23 +86,39 @@ window.addEventListener("load", function(){
                     </div></div>`;
 
                     document.querySelector(".workshops").innerHTML += workshops;
+
+                   
+                    let workshopData = json.data;
+                    let workshopdetail = document.querySelector(".workshops");
+                    workshopdetail.addEventListener("click", (e)=>{
+                        let index = [... workshopdetail.childNodes].indexOf(e.target.parentElement.parentElement)-3;
+                        console.log(index);
+                        if(index < 0){index = null}
+                        if(index != null){
+                            console.log(workshopData);
+                            console.log(workshopData[index]);
+                            console.log(workshopData[1]._id);
+                            localStorage.setItem("workshopID", JSON.stringify(workshopData[index]._id));
+                            window.location.replace("workshop_detail.html");
+                            console.log(workshopData);
+                        }
+                    });
                 });
             }
         })
-
     }
 });
 
 function splitdate(date){
     let datum = date.split("T")[0];
-    console.log(datum);
+    // console.log(datum);
     datum = datum.split("-");
     if(!datum[0]){datum[0] = " ";}
     if(!datum[1]){datum[1] = " ";}
     if(!datum[2]){datum[2] = " ";}
-    console.log(" 0 " + datum[0]);
-    console.log( " 1 " + datum[1]);
-    console.log( " 2 " + datum[2]);
+    // console.log(" 0 " + datum[0]);
+    // console.log( " 1 " + datum[1]);
+    // console.log( " 2 " + datum[2]);
 
     return datum;
 }
