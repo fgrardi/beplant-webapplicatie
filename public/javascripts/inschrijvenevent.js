04731e11b23d801906e476b1ad1526bcede4e4fe
SarahOorts <r0657122@student.thomasmore.be>
2021-06-04 12:39:03 +0200

    feat: add eventlistener on submit button to fetch a put request to update amount of inschrijvingen

--- public/javascripts/inschrijvenevent.js
+++ public/javascripts/inschrijvenevent.js
@@ -1,10 +1,10 @@
 let tokencheck = localStorage.getItem("token");
+let data = JSON.parse(localStorage.getItem("eventID"));
 window.addEventListener("load", function(){
     if(!tokencheck){
         window.location.replace("login.html");
     }
     else{
-        let data = JSON.parse(localStorage.getItem("eventID"));
         let titel = document.querySelector(".titel");
 
         if(data == undefined || data == null || data == " "){
@@ -36,6 +36,31 @@ window.addEventListener("load", function(){
                 }
             }
         })
-        localStorage.removeItem("eventID");
     }
 });
+
+let inschrijving = document.querySelector(".button--submit");
+inschrijving.addEventListener("click", () =>{
+    fetch(`/events/eventinschrijven/id=${data}`, {
+        method: "put",
+        headers: {
+            "Content-Type": "application/json",
+            'Authorization': `Bearer ${tokencheck}`
+        }
+    }).then(response =>{
+        console.log("done1");
+        return response.json();
+    }).then(json =>{
+        if(json.status === "Success"){
+            console.log(json);
+            let message = `<h1>Je bent ingeschreven voor dit event</h1>`
+            document.querySelector(".ingeschreven").innerHTML = message;
+
+        }
+        if(json.status === "Error"){
+            console.log("error");
+        }
+    })
+    // localStorage.removeItem("eventID");
+})
+
