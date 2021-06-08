a075360dacf42ada1d00ce0294cf7f5a5fa334cd
SarahOorts <r0657122@student.thomasmore.be>
2021-06-08 19:37:01 +0200

    feat: add errormessages to signup.js and changed window.location.replace to window.location.assign. Moved css files main to behind webapp.css in signup.html

--- public/javascripts/signup.js
+++ public/javascripts/signup.js
@@ -6,9 +6,44 @@ let signup = document.querySelector(".button--submit").addEventListener("click",
 
     let emailrestriction = email.indexOf("@");
 
-    if(emailrestriction === -1){
-        console.log("Incorrect email");
+    let first = document.querySelector(".firstname");
+    let last = document.querySelector(".lastname");
+    let em = document.querySelector(".email");
+    let pass = document.querySelector(".password");
+    let errorname = "<p class='invalid'>Niet alle velden zijn correct ingevuld, gelieve deze te vervolledigen</p>";
+    let erroremail = "<p class='invalid'>Het opgegeven e-mailadres kunnen we niet aanvaarden, gelieve opnieuw te proberen</p>";
+    let errorem = "<p class='invalid'>Het opgegeven e-mailadres is incorrect, gelieve opnieuw te proberen</p>";
+    let errorpass = "<p class='invalid'>Het opgegeven wachtwoord is incorrect, gelieve opnieuw te proberen</p>";
+
+    if(firstname == ""){
+        first.insertAdjacentHTML("beforebegin", errorname);
+        first.classList.add("error");
     }
+    else if(lastname == ""){
+        document.querySelector(".invalid").remove();
+        first.insertAdjacentHTML("beforebegin", errorname);
+        first.classList.remove("error");
+        last.classList.add("error");
+    }
+    else if(email == ""){
+        document.querySelector(".invalid").remove();
+        first.insertAdjacentHTML("beforebegin", errorem);
+        last.classList.remove("error");
+        em.classList.add("error");
+    }
+    else if(emailrestriction === -1){
+        // console.log("Incorrect email");
+        document.querySelector(".invalid").remove();
+        first.insertAdjacentHTML("beforebegin", erroremail);
+        em.classList.remove("error");
+        em.classList.add("error");
+    }
+    else if(password == ""){
+        document.querySelector(".invalid").remove();
+        first.insertAdjacentHTML("beforebegin", errorpass);
+        em.classList.remove("error");
+        pass.classList.add("error");
+    }    
     else{
         fetch("/users/mail", {
             method: "post",
@@ -44,7 +79,7 @@ let signup = document.querySelector(".button--submit").addEventListener("click",
         
                         let token = answer.data.token;
                         localStorage.setItem("token", token);
-                        window.location.replace("home.html");
+                        window.location.assign("home.html");
                     }
                 })
             }
@@ -54,5 +89,5 @@ let signup = document.querySelector(".button--submit").addEventListener("click",
 
 let back = document.querySelector(".back");
 back.addEventListener("click", () =>{
-    window.location.replace("start.html");
+    window.location.assign("start.html");
 });
