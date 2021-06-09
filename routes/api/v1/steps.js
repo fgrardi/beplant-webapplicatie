4ad3a0acb6c8a6b3f98070c51cfe26381b481615
SarahOorts <r0657122@student.thomasmore.be>
2021-06-09 09:54:30 +0200

    feat: add new route for new put request to update step 3

--- routes/api/v1/steps.js
+++ routes/api/v1/steps.js
@@ -5,5 +5,6 @@ const stepController = require("../../../controllers/api/v1/step");
 router.get("/", stepController.getSteps);
 router.put("/stappenplan", stepController.putstep);
 router.put("/reset", stepController.putreset);
+router.put("/stap3", stepController.putstep3);
 
 module.exports = router;
