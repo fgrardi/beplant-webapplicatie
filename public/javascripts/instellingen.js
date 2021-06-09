2fd19bdfde5ae3502adc7a6543a4ffbdba9063e2
SarahOorts <r0657122@student.thomasmore.be>
2021-06-09 14:09:18 +0200

    feat: create new js file for instellingen. Add eventlisteners for logout out and navigation. Changed layout instellingen.html

---
+++ public/javascripts/instellingen.js
@@ -0,0 +1,38 @@
+let menu = document.querySelector(".menu");
+let navigation = document.querySelector(".navigation");
+menu.addEventListener("click", ()=>{
+    document.querySelector(".nav").classList.toggle("hidden");
+    if(document.querySelector(".nav").classList.contains("hidden")){
+        menu.setAttribute("src", "./assets/menu-icon.png");
+        navigation.style.backgroundColor = "transparent";
+        // navigation.style.top = "30px";
+        navigation.style.position = "relative";
+        navigation.style.height = "auto";
+        // document.querySelector(".nav__flex").style.marginTop = "-10px";
+        // navigation.style.marginTop = "-20px";
+        // document.querySelector(".nav__flex").style.marginBottom = "80px";
+    }
+    else{
+        menu.setAttribute("src", "./assets/cross-icon.png");
+        navigation.style.backgroundColor = "#CAD3BD";
+        navigation.style.position = "fixed";
+        navigation.style.height = "100%";
+        navigation.style.top = "0px";
+        navigation.style.overflowX = "hidden";
+        navigation.style.zIndex = "5";
+        navigation.style.width = "315px";
+        // document.querySelector(".nav__flex").style.marginTop = "32px";
+        navigation.style.marginTop = "32px";
+    }
+    
+});
+
+let arrow = document.querySelector(".arrow");
+arrow.addEventListener("click", ()=>{
+    window.location.assign("home.html");
+})
+
+let logout = document.querySelector(".logout");
+logout.addEventListener("click", ()=>{
+    localStorage.removeItem("token");
+});
